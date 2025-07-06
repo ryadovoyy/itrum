7d308d17de3fe77664f05e8c550dec0bfa5e479b
ryadovoyy <holstinindvs@gmail.com>
2025-07-06 17:19:01 +0700

    initial commit

---
+++ src/main/java/com/ryadovoy/itrum/ItrumApplication.java
@@ -0,0 +1,11 @@
+package com.ryadovoy.itrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItrumApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItrumApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ryadovoy/itrum/ItrumApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ryadovoy.itrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItrumApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
